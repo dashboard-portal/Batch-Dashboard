05852084a70220f5dd39a0f7259945568e94ba04
genDecoder <mariomedranomaldonado@gmail.com>
2018-04-20 11:02:56 -0400

    Dependency added and set up using the hash approach completed.
    PD: As well the version of the react library was updated to the latest one.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
 import React from 'react'
+import {
+    HashRouter as Router
+} from 'react-router-dom'
 
 export default class extends React.Component {
     render () {
         return (           
+            <Router>
+                {
+                    this._app()
+                }
+            </Router>
+        );
+    }
+    _app () {
+        return (
+            <div
+
+            >
                 <div
+                    className='topbar'
                 >
                     <div
-                        className='topbar'
+                        className='welcometopbar'
                     >
                         <div
-                            className='welcometopbar'
+                            className='logo-container'
                         >
                             <div
-                                className='logo-container'
-                            >
-                                <div
-                                    className='logo'
-                                ></div>
-                            </div>
-                            <div>
-                                
-                            </div>
+                                className='logo'
+                            ></div>
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                    <div
+                        className='topmenu'
+                    >
+                        <div
+                            className='topmenu-item'
+                        >
+                            Home
                         </div>
                         <div
-                            className='topmenu'
+                            className='topmenu-item selected'
                         >
-                            <div
-                                className='topmenu-item'
-                            >
-                                Home
-                            </div>
-                            <div
-                                className='topmenu-item selected'
-                            >
-                                Images
-                            </div>
-                            <div
-                                className='topmenu-item'
-                            >
-                                Lectures
-                            </div>
+                            Images
                         </div>
-                    </div>                    
-                </div>                
+                        <div
+                            className='topmenu-item'
+                        >
+                            Lectures
+                        </div>
+                    </div>
+                </div>                    
+            </div>     
         );
     }
 }
